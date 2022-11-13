0aaf943d73b993803773dfc1b9789d9da081bdf9
Henrique Artur <contato@henriqueartur.com>
2022-11-13 16:53:18 -0300

    :wrench: Add Aura Theme colors

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,21 @@
 module.exports = {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {}
+    extend: {
+      colors: {
+        primary: "#a277ff",
+        secondary: "#61ffca",
+        tertiary: "#ffca85",
+        quaternary: "#f694ff",
+        quinary: "#82e2ff",
+        senary: "#ff6767",
+        selections: "#29263c",
+        selectionsAlpha: "#3d375e7f",
+        foregrounds: "#edecee",
+        comments: "#6d6d6d",
+        backgrounds: "#15141b",
+      }
+    }
   },
   plugins: []
 };
