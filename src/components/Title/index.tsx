f67cca1a0314a7d16dcc9d7734bed7d7c8bb2ff2
Henrique Artur <contato@henriqueartur.com>
2022-11-15 15:59:39 -0300

    :dizzy: add title typing animation

--- src/components/Title/index.tsx
+++ src/components/Title/index.tsx
@@ -1,4 +1,6 @@
+import { useEffect, useState } from "react";
 import useTitle from "../../hooks/useTitle";
+import Typewriter, { TypewriterClass } from 'typewriter-effect';
 
 export type Title =
   "Hello World"
@@ -8,10 +10,32 @@ export type Title =
 
 export default function Title(): JSX.Element {
   const title = useTitle();
+  const [writer, setWriter] = useState<TypewriterClass>();
+
+  const animate = (typewriter: any) => {
+    typewriter
+      .typeString(title)
+      .start();
+    setWriter(typewriter);
+  };
+
+  useEffect(() => {
+    if (writer) {
+      writer
+        .deleteAll(15)
+        .typeString(title)
+        .start()
+      setWriter(writer);
+    }
+  }, [title]);
 
   return (
-    <h1 className="mx-[60px] text-2xl">
-      // {title}
+    <h1 className="mx-[60px] text-4xl flex">
+      <p>//</p>
+      <Typewriter
+        onInit={animate}
+        options={{ cursor: ".", delay: 35 }}
+      />
     </h1>
   );
 };
